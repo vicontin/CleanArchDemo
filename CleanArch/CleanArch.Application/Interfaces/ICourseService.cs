b68a5231233b979b593dff0488aab1073f8fb9e9
Victor Contin <vcontin@live.com>
2020-06-13 12:15:21 -0700

    Course Service Create Functionality, Updated CourseViewModel

--- CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -7,6 +7,7 @@ namespace CleanArch.Application.Interfaces
     public interface ICourseService
     {
         CourseViewModel GetCourses();
+        void CreateCourse(CourseViewModel courseViewModel);
         
     }
 }
